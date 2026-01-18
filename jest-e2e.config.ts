dc471301fa6a563a4aab0b88291d9b1d02e1127d
davidbestel <davidbestel.dev@gmail.com>
2026-01-17 21:15:25 -0300

    feat: :sparkles: update environment configuration, add e2e testing setup, and modify dependencies

---
+++ jest-e2e.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from '@jest/types'
+import { pathsToModuleNameMapper } from 'ts-jest'
+import { compilerOptions } from './tsconfig.json'
+
+const config: Config.InitialOptions = {
+  rootDir: './',
+  testEnvironment: 'node',
+  testRegex: '.e2e-spec.ts$',
+  transform: { '^.+\\.(t|j)s$': 'ts-jest' },
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+  maxWorkers: 1,
+}
+
+export default config
